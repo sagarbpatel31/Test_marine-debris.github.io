80de4f7254c6fc1b67ae3fdfe036f90cc29b14d3
Sagar Patel <52515373+sagarbpatel31@users.noreply.github.com>
2025-03-30 22:46:49 -0700

    Update engineering_patches.py

--- semantic_segmentation/random_forest/engineering_patches.py
+++ semantic_segmentation/random_forest/engineering_patches.py
@@ -18,7 +18,7 @@ from functools import partial
 from os.path import dirname as up
 from skimage.color import rgb2gray
 from joblib import Parallel, delayed
-from skimage.feature import greycomatrix, greycoprops, local_binary_pattern
+from skimage.feature import graycomatrix, graycoprops, local_binary_pattern
 
 root_path = up(up(up(os.path.abspath(__file__))))
 
